7f31b647bf5d1c8dc15233673ff1fb224bd35e98
chyiyenwang <chyiyenwang@gmail.com>
2016-02-09 10:21:54 -0800

    changed some stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var path = require('path');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 app.use(ejsLayouts);
 app.use(express.static(path.join(__dirname, '/static')));
